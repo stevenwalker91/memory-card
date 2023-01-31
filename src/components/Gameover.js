2652d0c86c29e4d13d1fa109d355952f718a7423
Steven Walker <stevenwalker1991@googlemail.com>
2023-01-31 14:18:04 +0000

    created gameover component

---
+++ src/components/Gameover.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Gameover = ({status, newGame, score}) => {
+
+
+  let successMessage;
+  if (status === 'win') {
+    successMessage = <p>You won! 🎉🎉</p>
+  } else {
+    successMessage = <p>Well done, you made it all the way to {score}. Can you make it the entire way to the end though?! <span className="emoji">🤠</span></p>
+  }
+
+
+  return (
+    <main className="gameover">
+      {successMessage}
+      <button type="button" onClick={() => newGame()}>Play again</button>
+    </main>
+  )
+
+};
+
+Gameover.propTypes = {
+  status: PropTypes.string.isRequired,
+  newGame: PropTypes.func.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+
+export default Gameover;
