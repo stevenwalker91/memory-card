0b920ea5f57fbc57033b19f6780a0ec514dd4b97
Steven Walker <stevenwalker1991@googlemail.com>
2023-01-31 11:31:30 +0000

    created basic scoreboard that takes scores as props

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Scoreboard = () => {
+const Scoreboard = ({score, highScore}) => {
 
   return (
     <header className="scoreboard">
-        
+        <h1>Memory Game</h1>
+        <div className="score-container">
+          <span>{`Current Score: ${score}`}</span>
+          <span>{`High Score: ${highScore}`}</span>
+        </div>
     </header>
   )
 }
+
+Scoreboard.propTypes = {
+  score: PropTypes.number.isRequired,
+  highScore: PropTypes.number.isRequired
+};
+
 
 export default Scoreboard;
